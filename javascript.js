3b9d0ee9907678bf0c51f4c15ecfca3c387af625
Koverin <110396064+Koverin@users.noreply.github.com>
2022-10-25 19:16:46 +0300

    вникаю

---
+++ javascript.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Задание №3
+// const topSpeed = 160;
+// const distance = 617.54;
+
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed)
+
+
+// Задание №4
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// console.log(totalPrice)
+
+// Задание №5
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem}, credits`;
+
+// console.log(message)
+
+// Задание №6
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// console.log(message)
+
+// Задача №7
+
+// function sayHi() {
+//   console.log("Hello, this is my first function!")
+// }
+
+// sayHi()
+
+// Задача №8
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// Задача №9
+
+
